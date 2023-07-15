cede723bf1d1aa1c31c11241d752535a48a5e37a
bathkora <mukesh.bathkora@gmail.com>
2023-07-15 17:42:22 +0530

    Authentication commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require('cookie-parser');
 const {connectToMongoDB} = require("./connect");
+const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth');
+const URL = require('./models/url');
+
 const urlRoute = require("./routes/url");
 const staticRoute = require('./routes/staticRouter');
-const URL = require('./models/url');
+const userRoute = require('./routes/user');
 
 const app = express();
 
@@ -19,11 +23,13 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 
 
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get('/url/:shortId', async(req, res) => {
     const shortId = req.params.shortId;
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,10 +4,19 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async(req, res) => {
+    if(!req.user) return res.redirect("/login");
     const allurls = await URL.find({});
     return res.render("home", {
         urls: allurls,
     });
+});
+
+router.get("/signup", (req, res) => {
+    return res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+    return res.render("login");
 });
 
 module.exports = router;
